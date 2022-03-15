5240fab9ebe2658340cd95aec9436099c7df580a
Eduardo <eduardoguette@gmail.com>
2022-03-15 22:13:31 +0100

    ADD FEATURES

--- src/components/ui/UserMenu.js
+++ src/components/ui/UserMenu.js
@@ -1,5 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion'; 
 import { useDispatch, useSelector } from 'react-redux';
 import { startLogOut } from '../../actions/auth';
 import user from '../../assets/icons/user.svg';
@@ -10,30 +9,24 @@ export const UserMenu = () => {
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(startLogOut());
-  };
-  const [showMenu, setShowMenu] = useState(false);
+  }; 
 
   return (
-    <motion.div layout className='relative group' onHoverEnd={() => setShowMenu(!showMenu)} onHoverStart={() => setShowMenu(!showMenu)}>
+    <motion.div layout className='relative group'>
       <button className='flex items-center gap-2 px-4 py-3 text-sm transition-all ease-linear'>
         <img src={user} alt='Icono sol' height={20} width={20} />
       </button>
       <AnimatePresence>
-        {showMenu && (
-          <motion.ul
-       
-            className='absolute -bottom-[160%] hidden group-hover:flex  flex-col gap-1 pt-2 pb-1 bg-white border rounded shadow-md position right-[3%] w-max'
-          >
-            <li>
-              <span className='px-4 py-2 text-sm font-light '> {name}</span>
-            </li>
-            <li>
-              <button onClick={handleLogout} className='w-full px-4 py-2 text-sm font-light text-left hover:bg-indigo-200'>
-                Logout
-              </button>
-            </li>
-          </motion.ul>
-        )}
+        <motion.ul className='absolute -bottom-[160%] hidden group-hover:flex  flex-col gap-1 pt-2 pb-1 bg-white border rounded shadow-md position right-[3%] w-max'>
+          <li>
+            <span className='px-4 py-2 text-sm font-light '> {name}</span>
+          </li>
+          <li>
+            <button onClick={handleLogout} className='w-full px-4 py-2 text-sm font-light text-left hover:bg-indigo-200'>
+              Logout
+            </button>
+          </li>
+        </motion.ul>
       </AnimatePresence>
     </motion.div>
   );
