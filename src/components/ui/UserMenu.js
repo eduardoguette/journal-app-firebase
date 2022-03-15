f21531a50cf6428ae012f644f09fb87938a21765
Eduardo <eduardoguette@gmail.com>
2022-03-15 22:08:11 +0100

    ADD FEATURES

--- src/components/ui/UserMenu.js
+++ src/components/ui/UserMenu.js
@@ -14,18 +14,15 @@ export const UserMenu = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <motion.div layout className='relative ' onHoverEnd={() => setShowMenu(!showMenu)} onHoverStart={() => setShowMenu(!showMenu)}>
+    <motion.div layout className='relative group' onHoverEnd={() => setShowMenu(!showMenu)} onHoverStart={() => setShowMenu(!showMenu)}>
       <button className='flex items-center gap-2 px-4 py-3 text-sm transition-all ease-linear'>
         <img src={user} alt='Icono sol' height={20} width={20} />
       </button>
       <AnimatePresence>
         {showMenu && (
           <motion.ul
-            initial={{ scale: 0, translateY:"-50%" }}
-            animate={{ scale: 1,translateY:"0%"}}
-            exit={{ scale: 0,translateY:"-50%" }}
-            transition={{ type: 'spring', duration: 0.4 }}
-            className='absolute -bottom-[160%]   flex-col gap-1 pt-2 pb-1 bg-white border rounded shadow-md position right-[3%] w-max'
+       
+            className='absolute -bottom-[160%] hidden group-hover:flex  flex-col gap-1 pt-2 pb-1 bg-white border rounded shadow-md position right-[3%] w-max'
           >
             <li>
               <span className='px-4 py-2 text-sm font-light '> {name}</span>
